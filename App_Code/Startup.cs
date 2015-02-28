0e0658c0cd4bc01d9283ef4808912078498d6079
Marc <marc@blackhawkdown.org.uk>
2015-02-28 22:24:29 +0000

    first commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wedding.Startup))]
+namespace wedding
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
