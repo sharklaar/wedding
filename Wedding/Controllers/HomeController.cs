a434d8ceac19c79387db1ff5ef945a53f7488478
Marc <marc@blackhawkdown.org.uk>
2015-04-13 21:21:24 +0100

    V2 first commit

---
+++ Wedding/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wedding.Helpers;
+using Wedding.Models;
+
+namespace ASP.NET_MVC5_Bootstrap3_3_1_LESS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Rsvp(Guest guest)
+        {
+            var helper = new DatabaseHelper();
+
+            var guests = new GuestList();
+            guests.Guests = helper.GetGuestsWithUsername(guest.Username);
+           
+            return View(guests);
+        }
+
+        [HttpPost]
+        public ActionResult SubmitGuests(GuestList guests)
+        {
+            if (guests == null)
+                return View();
+
+            var helper = new DatabaseHelper();
+
+            helper.SubmitGuestDetails(guests);
+
+            return View();
+        }
+    }
+}
---
+++ Wedding/Helpers/DatabaseHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Wedding.Models;
+
+namespace Wedding.Helpers
+{
+    public class DatabaseHelper
+    {
+        private string _connectionString;
+
+        public DatabaseHelper()
+        {
+
+        }
+
+        public void SubmitGuestDetails(GuestList guests)
+        {
+            
+        }
+
+        public List<Guest> GetGuestsWithUsername(string username)
+        {
+            var GuestList = new List<Guest>();
+
+            _connectionString = ConfigurationManager.ConnectionStrings["WeddingDb"].ConnectionString;
+            var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            try
+            {
+                var cmd = new SqlCommand("GetGuests", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Username", username);
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var guest = new Guest();
+
+                    guest.Username = username;
+                    guest.Id = int.Parse(reader["ID"].ToString());
+                    guest.Name = reader["Name"].ToString();
+                    guest.AttendingCeremony = GetBooleanFromBit(reader["AttendingCeremony"].ToString());
+                    guest.AttendingMeal = GetBooleanFromBit(reader["AttendingMeal"].ToString());
+                    guest.AttendingReception = GetBooleanFromBit(reader["AttendingReception"].ToString());
+                    guest.CeremonyPermitted = GetBooleanFromBit(reader["CeremonyPermitted"].ToString());
+                    guest.MealPermitted = GetBooleanFromBit(reader["MealPermitted"].ToString());
+                    guest.ReceptionPermitted = GetBooleanFromBit(reader["ReceptionPermitted"].ToString());
+                    guest.Dessert = reader["Dessert"].ToString();
+                    guest.Main = reader["Main"].ToString();
+                    guest.Starter = reader["Starter"].ToString();
+
+                    GuestList.Add(guest);
+                }
+
+                return GuestList;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private bool GetBooleanFromBit(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            if (input == "1")
+                return true;
+
+            if (input == "true")
+                return true;
+
+            if (input == "True")
+                return true;
+
+            return false;
+        }
+    }
+}
